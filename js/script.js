591161a66530e5969a489b4481334a5ebbd70281
TestMan <maext@protonmail.com>
2022-05-20 16:24:23 +0200

    Arranging the code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
-import { showHideMenu } from "../scss/mobile/typography/nav/__button/_toggle-menu/_toggle-menu.js";
-import { changeMainImg } from "../scss/mobile/typography/main/__image/_size-small/_size-small.js";
-import { fixedHeader } from "../scss/common/page/__header/_pos_fixed/_pos_fixed.js";
+import { showHideMenu } from "../scss/mobile/look/nav/__button/_toggle-menu/_toggle-menu.js";
+import { changeMainImg } from "../scss/mobile/look/main/__image/_size-small/_size-small.js";
+import { fixedHeader } from "../scss/common/typography/page/__header/_pos_fixed/_pos_fixed.js";
